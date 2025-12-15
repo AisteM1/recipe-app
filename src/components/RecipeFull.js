e8a83504c72d99a898d51d2950508a469cdf8bb6
aiste_miskinis <aiste.miskinis@gmail.com>
2025-12-15 16:42:50 -0600

    Updated RecipeFull.js

--- src/components/RecipeFull.js
+++ src/components/RecipeFull.js
@@ -69,9 +69,11 @@ const RecipeFull = ({
 
           <ul className="ingredient-list">
             {selectedRecipe.ingredients.split(",").map((ingredient, index) => {
+              return (
               <li className="ingredient" key={index}>
                 {ingredient}
-              </li>;
+              </li>
+              );
             })}
           </ul>
           <h3>Instructions:</h3>
